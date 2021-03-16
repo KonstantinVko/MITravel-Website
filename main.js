c4deec0192c8492778951c5bc38d836c66498933
Konstanta <anastasia.corporate@gmail.com>
2021-03-16 17:14:01 +0300

    vids + stories

--- main.js
+++ main.js
@@ -1,8 +1,5 @@
 let swiper = new Swiper('.swiper-container', {
     effect: 'coverflow',
-    loop: true,
-    loopedSlides: 3,
-    grabCursor: true,
     centeredSlides: true,
     slidesPerView: 'auto',
     coverflowEffect: {
@@ -15,9 +12,42 @@ let swiper = new Swiper('.swiper-container', {
 });
 
 
-
 function changeBlock() {
-   let block = document.getElementById('stories');
-   block.style.background = "url('images/Surf-2.webp') no-repeat";
-   block.style.backgroundSize = 'cover';
+    let block = document.getElementById('stories');
+    block.style.background = "url('images/Surf-2.webp') no-repeat";
+    block.style.backgroundSize = 'cover';
+}
+
+
+// Get the video
+let video1 = document.getElementById('beach_vid');
+let video2 = document.getElementById('muntains_vid');
+let video3 = document.getElementById('sand_vid');
+
+
+
+// Get the button
+let btn = document.getElementById("myBtn");
+
+// Pause and play the video, and change the button text
+function playVideo1() {
+    if (video1.paused) {
+        video1.play();
+    } else {
+        video1.pause();
+    }
+}
+function playVideo2() {
+    if (video2.paused) {
+        video2.play();
+    } else {
+        video2.pause();
+    }
+}
+function playVideo3() {
+    if (video3.paused) {
+        video3.play();
+    } else {
+        video3.pause();
+    }
 }
